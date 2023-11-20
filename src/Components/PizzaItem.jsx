693c4a9696850d55f307fa93ea5baf437b6fceda
NAUT5 <Mohamedtlili1995@hotmail.com>
2023-11-20 16:59:02 +0100

    final-touch

--- src/Components/PizzaItem.jsx
+++ src/Components/PizzaItem.jsx
@@ -1,19 +1,34 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import PropTypes from 'prop-types'; // ES6
 
-function PizzaItem(props) {
+
+
+function PizzaItem({img ,title,price}) {
   return (
     <div>
       <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={props.img} />
+        <Card.Img variant="top" src={img} />
         <Card.Body>
-          <Card.Title>{props.title} </Card.Title>
-          <Card.Text> Price :{props.price} $</Card.Text>
+          <Card.Title>{title} </Card.Title>
+          <Card.Text> Price :{price} $</Card.Text>
           <Button variant="primary">Order now</Button>
         </Card.Body>
       </Card>
     </div>
   );
 }
+PizzaItem.defaultProps = {
+  img: "https://cdn-icons-png.flaticon.com/512/6978/6978255.png",
+  title: "Pizza name",
+  price: "N/A",
+};
+
+
+PizzaItem.propTypes = {
+  img: PropTypes.string,
+  title: PropTypes.string,
+  price: PropTypes.number,
+};
 
 export default PizzaItem;
--- src/Components/PizzaList.jsx
+++ src/Components/PizzaList.jsx
@@ -1,28 +1,12 @@
+import React from "react";
 import PizzaItem from "./PizzaItem";
-
+import { data } from "../data";
 function PizzaList() {
   return (
-    <div>
-      <PizzaItem
-        img="https://www.allrecipes.com/thmb/fFW1o307WSqFFYQ3-QXYVpnFj6E=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/48727-Mikes-homemade-pizza-DDMFS-beauty-4x3-BG-2974-a7a9842c14e34ca699f3b7d7143256cf.jpg"
-        title="Mike's Homemade Pizza"
-        price={10}
-      />
-      <PizzaItem
-        img="https://hips.hearstapps.com/hmg-prod/images/classic-cheese-pizza-recipe-2-64429a0cb408b.jpg?crop=0.6666666666666667xw:1xh;center,top&resize=1200:*"
-        title="Classic Cheese Pizza"
-        price={15}
-      />
-      <PizzaItem
-        img="https://www.foodandwine.com/thmb/Wd4lBRZz3X_8qBr69UOu2m7I2iw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/classic-cheese-pizza-FT-RECIPE0422-31a2c938fc2546c9a07b7011658cfd05.jpg"
-        title="Classic Pizza"
-        price={25}
-      />
-      <PizzaItem
-        img="https://cdn.loveandlemons.com/wp-content/uploads/2023/02/vegetarian-pizza-500x500.jpg"
-        title="Vegetarian Pizza"
-        price={16}
-      />
+    <div className="pizza-container">
+      {data.map((pizza, index) => (
+        <PizzaItem key={index} {...pizza} />
+      ))}
     </div>
   );
 }
